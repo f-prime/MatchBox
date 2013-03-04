ed9edf25d3eeadf4e94ba9b25e99b170c5bda911
gzuykov <gzuykov@auto.ru>
2013-03-04 14:21:49 +0400

    Many improvements
    Some PEP8 compiants, file upload rewritten to POST request, server uses
    Flask request to get files, unified command URL mask, overall speed
    improvements.
    WARNING: Totaly untested!

--- server.py
+++ server.py
@@ -1,59 +1,47 @@
-from flask import Flask
-import os, hashlib
+from flask import Flask, request
+from hashlib import sha1
+import os
 
 app = Flask(__name__)
-
-users = {
-
-'username':'password',
-
-        }
+users = {'username':'password'}
 
 @app.route("/delete/<username>/<password>/<file>")
 def delete(username, password, file):
-    if login(username, password) is False:
-        return "Login Failed"
-    else:
+    if login(username, password):
         os.remove("files/"+file)
         return "Success"
+    return "Login Failed"
 
-@app.route("/upload/<username>/<password>/<file>/<data>")
+@app.route("/upload/<username>/<password>/<file>", methods=['POST'])
 def upload(username, password, file, data):
-    if login(username, password) is False:
-        return "Login Failed"
-    else:
+    if login(username, password):
+        f = request.files[file]
         if file not in os.listdir("files"):
-            with open("files/"+file, 'w') as file:
-                pass
-        upload = open("files/"+file, 'ab')
-        data = data.split()
-        for data in data:
-            upload.write(chr(int(data)))
-        upload.close()
+            f.save("files/"+file)
+        else:
+            upload = open("files/"+file, 'ab')
+            upload.write(f.read())
+            upload.close()
         return "Success"
+    return "Login Failed"
 
 @app.route("/download/<username>/<password>/<file>")
 def download(username, password, file):
-    if login(username, password) is False:
-        return "Login Failed"
-    else:
+    if login(username, password):
         with open("files/"+file, 'rb') as file:
             return file.read()
+    return "Login Failed"
         
 @app.route("/list/<username>/<password>")
 def list(username, password):
-    if login(username, password) is False:
-        return "Login Failed"
-    else:
+    if login(username, password):
         return str(os.listdir("files"))
+    return "Login Failed"
 
 def login(username, password):
-    if username not in users:
-        return False
-    elif hashlib.sha1(users[username]).hexdigest() == password:
+    if username in users and sha1(users[username]).hexdigest() == password:
         return True
-    else:
-        return False
+    return False
 
 if __name__ == "__main__":
     if "files" not in os.listdir(os.getcwd()):
